af6c4a0393ebc8ef32a1cb3aecd7b37e88d14ad0
brucetoo <brucetoo14@gmail.com>
2016-06-15 17:31:50 +0800

    still don't understand why choose atan algorithm

--- CarouselLayoutManager/carousel/src/main/java/com/azoft/carousellayoutmanager/CarouselZoomPostLayoutListener.java
+++ CarouselLayoutManager/carousel/src/main/java/com/azoft/carousellayoutmanager/CarouselZoomPostLayoutListener.java
@@ -12,7 +12,17 @@ public class CarouselZoomPostLayoutListener implements CarouselLayoutManager.Pos
 
     @Override
     public ItemTransformation transformChild(@NonNull final View child, final float itemPositionToCenterDiff, final int orientation) {
-        final float scale = (float) (4 * (2 * -StrictMath.atan(Math.abs(itemPositionToCenterDiff) + 1.0) / Math.PI + 1) / 2);
+        //此算法想得到一个数学范围内的值而设计的及 (0, 1)
+        /**itemPositionToCenterDiff 代表的值的绝对值遇到 离center item的位置越大 scale系数越大
+         * atan(x)值的范围是(-Pi/2,Pi/2) x值越大
+         * Math.abs(itemPositionToCenterDiff) + 1.0 > 0    +1代表至少是 y = Pi/4 的线
+         * StrictMath.atan(Math.abs(itemPositionToCenterDiff) + 1.0) / Math.PI -- (Pi/4,Pi/2)
+         *
+         * 卧槽 ....还是搞不懂为什么要选择此种算法..
+         */
+        double v = 2 * -StrictMath.atan(Math.abs(itemPositionToCenterDiff) + 1.0) / Math.PI + 1;
+        double v1 = StrictMath.atan(Math.abs(itemPositionToCenterDiff)) / Math.PI;
+        final float scale = (float) (2 * v);
 
         // because scaling will make view smaller in its center, then we should move this item to the top or bottom to make it visible
         final float translateY;
